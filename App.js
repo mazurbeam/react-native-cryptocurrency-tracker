e95ba7ca5b90e42faef6c577dbf697a530616b61
mazurbeam <mazurbeam@gmail.com>
2018-01-03 20:34:52 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Provider } from 'react-redux'
+
+
+import Store from './src/Store'
+import { Header, CryptoContainer } from './src/components'
+
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <Provider store={Store}>
+        <View>
+          <Header />
+          <CryptoContainer />
+        </View>
+      </Provider>
+    )
+  }
+}
